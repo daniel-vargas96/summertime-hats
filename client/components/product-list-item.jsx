6ecc5286cec7f982695e0dd8360ee7b0444027b7
Daniel Vargas <dannyvargashb@gmail.com>
2020-07-23 16:05:39 -0700

    Created product list component

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -4,8 +4,8 @@ export default class ProductListItem extends React.Component {
 
   render() {
     return (
-      <div className="card" style="width: 18rem;">
-        <img className="card-img-top" src="..." alt="Card image cap"></img>
+      <div className="card">
+        <img className="card-img-top" src='../../server/public/images/shake-weight.jpg'></img>
         <div className="card-body">
           <h5 className="card-title">Card title</h5>
           <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import Header from './components/header';
-import ProductListItem from './components/product-list-item';
 
 ReactDOM.render(
-  <Header />,
-  <ProductListItem />,
   <App />,
   document.querySelector('#root')
 );
