cdba9dbc96c213470292627a355844604fbeb4eb
Daniel Vargas <dannyvargashb@gmail.com>
2020-07-23 14:48:42 -0700

    Created Header component

---
+++ client/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Wicked Sales</h1>
+        </header>
+      </div>
+    );
+  }
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import Header from './components/header';
 
 ReactDOM.render(
+  <Header />,
   <App />,
   document.querySelector('#root')
 );
