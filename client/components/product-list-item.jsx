c10c4262b9b83d902055b017b338f4e22e2faba7
Daniel Vargas <dannyvargashb@gmail.com>
2020-07-23 15:16:34 -0700

    Create Product List Item component

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+export default class ProductListItem extends React.Component {
+
+  render() {
+    return (
+      <div className="card" style="width: 18rem;">
+        <img className="card-img-top" src="..." alt="Card image cap"></img>
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+      </div>
+    );
+  }
+}
