95e87ecdc6bbb9c88006cfdd2f4aee13047703dd
Daniel Vargas <dannyvargashb@gmail.com>
2020-08-25 11:44:25 -0700

    defined a stateless CartSummary component

---
+++ client/components/cart-summary.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import CartSummaryItem from './cart-summary-item';
+
+export default function CartSummary(props) {
+  const setView = props.setView;
+  const cart = props.cart;
+  const total = cart.reduce((sum, item) => {
+    return sum + item.price;
+  }, 0);
+
+  const cartItems = cart.map(item =>
+    <CartSummaryItem key={item.cartItemId} item={item} />
+  );
+
+  if (cart.length === 0) {
+    return (
+      <div className='container'>
+        <span
+          onClick={() => setView('catalog', {})}
+          className="text-muted ml-1 cursor-pointer">
+          {'< Back to catalog'}
+        </span>
+        <h3 className="my-3">My Cart</h3>
+      </div>
+    );
+  } else {
+    return (
+      <div className='container'>
+        <span
+          onClick={() => setView('catalog', {})}
+          className="text-muted ml-1 cursor-pointer">
+          {'< Back to catalog'}
+        </span>
+        <h3 className="my-3">My Cart</h3>
+        <div className="container">
+          {cartItems}
+        </div>
+        <h4 className="mt-3">Item Total {'$' + (total / 100).toFixed(2)}</h4>
+      </div>
+    );
+  }
+}
