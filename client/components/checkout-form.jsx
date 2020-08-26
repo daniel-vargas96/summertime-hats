09bb431ecb37e889e1b7f8b28d6deaf41512bfac
Daniel Vargas <dannyvargashb@gmail.com>
2020-08-26 10:58:11 -0700

    defined stateful Checkout Form component

---
+++ client/components/checkout-form.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+export default class CheckoutForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      creditCard: '',
+      shippingAddress: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const placeOrder = this.props.placeOrder;
+    placeOrder(this.state);
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  render() {
+    const setView = this.props.setView;
+    const cart = this.props.cart;
+    const totalPrice = cart.reduce((sum, item) => {
+      return sum + item.price;
+    }, 0);
+    return (
+      <div className='container'>
+        <form onSubmit={this.handleSubmit}>
+          <h1 mt-4>My Cart</h1>
+          <h4 text-muted my-4>Order Total: ${(totalPrice / 100).toFixed(2)}</h4>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input name='name' type='text' value={this.state.name} onChange={this.handleChange} className='form-control'></input>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='credit-card'>Credit Card</label>
+            <input name='creditCard' type='text' value={this.state.creditCard} onChange={this.handleChange} className='form-control'></input>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='shipping-address'>Shipping Address</label>
+            <textarea name='creditCard' type='text' value={this.state.shippingAddress} onChange={this.handleChange} className='form-control'></textarea>
+          </div>
+          <div className="d-flex justify-content-between">
+            <span onClick={() => setView('catalog', {})} className="text-muted cursor-pointer">{'< Continue Shopping'}</span>
+            <button type="submit" className="btn btn-primary">Place Order</button>
+          </div>
+        </form>
+      </div>
+    );
+
+  }
+}
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -6,14 +6,16 @@ export default class Header extends React.Component {
     const setView = this.props.setView;
     return (
       <div>
-        <header className='container-fluid row'>
-          <div className='col-10'>
-            <h5>$Wicked Sales</h5>
-          </div>
-          <div className='col-2'>
-            <div onClick={() => setView('cart', {})} className="d-flex align-items-center text-white pointer">
-              <span className="mr-2">{this.props.cartItemCount} Items</span>
-              <i className="fas fa-shopping-cart cart-icon"></i>
+        <header className='container-fluid'>
+          <div className='row'>
+            <div className='col-10'>
+              <h5>$Wicked Sales</h5>
+            </div>
+            <div className='col-2'>
+              <div onClick={() => setView('cart', {})} className="d-flex align-items-center text-white pointer">
+                <span className="mr-2">{this.props.cartItemCount} Items</span>
+                <i className="fas fa-shopping-cart cart-icon"></i>
+              </div>
             </div>
           </div>
         </header>
