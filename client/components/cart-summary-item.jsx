7ca01ac2b177217cab4848573d4f7e47ead1eb8b
Daniel Vargas <dannyvargashb@gmail.com>
2020-09-02 14:25:49 -0700

    updated m

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -5,7 +5,7 @@ export default function CartSummaryItem(props) {
 
   return (
     <div className='row border bg-white shadow-sm my-3 p-4'>
-      <img src={item.image} alt={item.name} className='col-md-4 object-fit'></img>
+      <img src={item.image} alt={item.name} className='col-md-4 mobile-images'></img>
       <div className="card-body d-flex flex-column justify-content-center col-md-8 item-padding">
         <h4 className="card-title">{item.name}</h4>
         <h6 className="card-text text-muted">{'$' + (item.price / 100).toFixed(2)}</h6>
